7bed64e7ced192f013f358e413275eaeede02103
Raquel Wetzel <raquelwetzel@gmail.com>
2022-07-03 12:03:19 +0100

    disable eslint

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import { SubHeading } from '../../components';
-import{images} from '../../constants'
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div className='app__header app__wrapper section__padding' id='home'>
+  <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour"/>
-      <h1 className='app__header-h1'>
-        The Key to Fine Dining
-      </h1>
-      <p className="p__opensans" style={{margin:'2rem 0'}}>
-      Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
       </p>
-      <button type='button' className='custom__button'>Explore Menu</button>
-
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
     <div className="app__wrapper_img">
-      <img src={images.welcome} style={{border:"10px solid var(--color-golden)"}} alt ="header img" />
-
+      <img
+        src={images.welcome}
+        style={{ border: "10px solid var(--color-golden)" }}
+        alt="header img"
+      />
     </div>
   </div>
 );
